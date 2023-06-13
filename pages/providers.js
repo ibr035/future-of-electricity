09f26ba5e745a66a385d7b8e99818c7d9b02150c
Ingri <ibrendsdal@gmail.com>
2023-06-13 23:36:20 +0200

    print out the provider data

--- pages/providers.js
+++ pages/providers.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "../Styles/Chart.module.css";
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Label } from "recharts";
 import { useEffect, useState } from "react";
 
@@ -11,18 +10,43 @@ import data from "../data/providers.json";
 const providerData = data.map((item) => {
     return {
         name: item.name,
-        pricing: item.pricing,
+        pricingModel: item.pricingModel,
         monthlyFee: item.monthlyFee,
         fixedPrice: item.fixedPrice,
         fixedPricePeriod: item.fixedPricePeriod,
+        variablePrice: item.variablePrice,
+        variablePricePeriod: item.variablePricePeriod,
+        spotPrice: item.spotPrice,
+
     };
 });
 const providers = () => {
-    return (
-        <div className={styles.container}>
-            < LineChart id="123" width={1000} height={400} data={providerData}>
+    const item = providerData.map((product, index) => (
+        <div key={index} className="card-item">
+            <h2 className="h2"> {product.name}</h2>
+            <div>pricingModel: {product.pricingModel}</div>
+            <div>monthlyFee: {product.monthlyFee}</div>
+            <div>fixedPrice: {product.fixedPrice}</div>
+            <div>fixedPricePeriod: {product.fixedPricePeriod}</div>
+            <div>variablePrice: {product.variablePrice}</div>
+            <div>spotPrice: {product.spotPrice}</div>            
+        </div>
 
-            </LineChart>
+    ))
+
+    return (
+
+
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-36">
+            <h1 className="text-l font-bold tracking-tight text-gray-900 sm:text-4xl">The different providers</h1>
+            <ul >
+                {console.log(providerData)}
+                <div className="card"> 
+                    {item}
+                </div>
+
+
+            </ul>
         </div>
     );
 }
